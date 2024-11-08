926684771b6f7d90d145eef9ff816dbcc9bc5a77
ParkerS00 <parkerswenson00@gmail.com>
2024-11-07 17:27:06 -0700

    Half way through the user backend stuff

---
+++ DiabloCharacterTracker.Server/Services/UserAccountServices/IUserAccountService.cs
@@ -0,0 +1,10 @@
+﻿using DiabloCharacterTracker.Server.DTOs;
+
+namespace DiabloCharacterTracker.Server.Services.UserAccountServices;
+
+public interface IUserAccountService
+{
+    public Task<UserAccountDTO> GetUserById(int userId);
+    public Task<UserAccountDTO> GetUserByEmail(string email);
+    public Task<int> AddUser();
+}
