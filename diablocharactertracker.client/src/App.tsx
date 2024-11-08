24676f5c6cbd89634e0a7168eeab56cea88da740
ParkerS00 <parkerswenson00@gmail.com>
2024-11-08 13:08:45 -0700

    Styling of the home page

--- diablocharactertracker.client/src/App.tsx
+++ diablocharactertracker.client/src/App.tsx
@@ -3,15 +3,26 @@ import "./Styles/Global.css";
 
 function App() {
   return (
-    <div className="h-screen bg-gradient-to-b from-blood-800 to-blood-950">
-      <Navbar></Navbar>
-      <div className="flex mt-16">
-        <div>
+    <div className="h-screen bg-gradient-to-b from-blood-800 to-blood-950 flex flex-col">
+      <Navbar />
+      <div className="flex flex-grow lg:justify-center items-center gap-16 px-16 mt-16 flex-col lg:flex-row">
+        <div className="flex flex-col text-center lg:text-left lg:w-1/2">
+          <p className="text-blood-50 text-wrap text-xl">
+            Welcome to the diablo character tracker, create and track all of
+            your different diablo 4 builds here! We have all 6 classes currenlty
+            playable in diablo 4 including the Barbarian, Druid, Srocerer,
+            Necromancer, Rogue, and the new Spiritborn.
+          </p>
+          <button className="block mt-4 px-4 py-2 bg-blood-600 text-blood-50 rounded-lg shadow-md hover:bg-blood-800">
+            Learn More
+          </button>
+        </div>
+        <div className="flex justify-center items-center w-auto h-auto">
           <img
-            className="w-full h-auto"
+            className="h-full w-full object-contain shadow-lg rounded-lg"
             src="/D4_LilithWallpaper_1920x1200_16x10_png_jpgcopy.jpg"
-            alt="Lillith"
-          ></img>
+            alt="Lilith"
+          />
         </div>
       </div>
     </div>
